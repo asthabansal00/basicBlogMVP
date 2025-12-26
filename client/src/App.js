ccab1f507d73b3c8fe7e114215c775a9ad886761
Astha Bansal <138471247+asthabansal00@users.noreply.github.com>
2025-12-26 21:36:11 +0530

    Reset the frontend(client)
    Reinitialized the client due to corrupted file upload

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Blog Frontend is Running!</h1>
+      <p>Test 1: Basic Rendering</p>
+    </div>
+  );
+}
+
+export default App;
+ 
